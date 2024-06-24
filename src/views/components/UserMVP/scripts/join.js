d369d1fec7efacb8c3ad75f27b552eb77d07366b
[AI11] 김민지 <kkimming__@naver.com>
2024-06-25 03:27:21 +0900

    feat: 이메일 인증 구현 중

--- src/views/components/UserMVP/scripts/join.js
+++ src/views/components/UserMVP/scripts/join.js
@@ -1,13 +1,18 @@
 // const userInfo = {};
+// import
 const joinForm = document.getElementById("join-form");
 let passwordMessage = document.getElementById("pw-check-message");
 const [
   joinInputEmail,
   joinInputDomain,
+  joinInputEmailCheck,
   joinInputPass1,
   joinInputPass2,
   joinInputName,
 ] = document.querySelectorAll("[name=join]");
+const emailReqBtn = document.getElementById("email-reqbtn");
+const emailResBtn = document.getElementById("email-resbtn");
+const emailMessage = document.getElementById("email-check-message");
 
 // 폼 제출 시 api 요청
 function onJoinSubmit(event) {
@@ -17,8 +22,6 @@ function onJoinSubmit(event) {
   // 빈칸 경고, 비밀번호 일치 확인
   if (joinInputEmail.value === "" || joinInputDomain.value === "") {
     return alert("이메일을 입력해주세요!");
-  } else if (joinInputDomain.value[0] !== "@") {
-    return alert("이메일 도메인은 @를 포함해야 합니다!");
   } else if (joinInputPass1.value === "") {
     return alert("비밀번호를 입력해주세요!");
   } else if (joinInputPass2.value === "") {
