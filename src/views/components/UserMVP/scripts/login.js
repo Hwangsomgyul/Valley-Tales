a3720135762d9dc24d4e24c5c933506c83f477d2
[AI11] 김민지 <kkimming__@naver.com>
2024-06-24 18:00:28 +0900

    feat: login 실패 시 경고창, api 리다이렉트

--- src/views/components/UserMVP/scripts/login.js
+++ src/views/components/UserMVP/scripts/login.js
@@ -1,22 +1,28 @@
-const loginForm = document.getElementById("login-form");
-const loginInputEmail = document.getElementById("login-email");
-const loginInputPassword = document.getElementById("login-password");
-const loginButton = document.getElementById("login-btn");
 const joinButton = document.getElementById("join-btn-at-login");
+
+const [
+  loginForm,
+  loginInputEmail,
+  loginInputDomain,
+  loginInputPassword,
+  loginButton,
+] = document.querySelectorAll("[id^=login-]");
 
 // 로그인 버튼 submit event
 function onLoginSubmit(event) {
   event.preventDefault();
   console.log("로그인 제출");
-  const loginEmail = loginInputEmail.value;
+  const loginEmail = loginInputEmail.value + loginInputDomain.value;
   const loginPassword = loginInputPassword.value;
-  if (loginEmail === "") {
+  if (loginInputEmail.value === "" || loginInputDomain.value === "") {
     return alert("이메일을 입력해주세요!");
   } else if (loginPassword === "") {
     return alert("비밀번호를 입력해주세요!");
   }
   console.log(loginEmail);
   console.log(loginPassword);
+
+  // api POST 요청
   fetchPostLogin(loginEmail, loginPassword);
 }
 
@@ -26,42 +32,45 @@ function fetchPostLogin(loginEmail, loginPassword) {
     headers: {
       "Content-Type": "application/json",
     },
+    credentials: "include",
     body: JSON.stringify({
       email: loginEmail,
       password: loginPassword,
     }),
-  })
-    .then((res) => {
-      console.log(res);
-      if (!res.ok) {
-        console.log("response error");
-        throw new Error("네트워크 응답이 올바르지 않습니다.");
-      }
-      return res.json();
-    })
-    // 로그인 응답 어떻게 들어오는지 보고 쓰기
-    .then((res) => {
-      if (res) {
-        console.log(res);
-      }
-    });
+  }).then((res) => {
+    console.log(res);
+    if (res.redirected) {
+      return (window.location.href = `/api/auth/users/${req.user.userId}`);
+    } else if (res.status === 400) {
+      return alert(`error : 이메일 또는 비밀번호를 확인해 주세요!`);
+    }
+    return alert(`오류가 발생했습니다.`);
+  });
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
 
-// // text 정보만 사용 가능
+// text 정보만 요청 baseApi
+// const BASE_API_URL = "/api";
 // const nonBodyMethods = ["GET", "DELETE"];
-// const baseApi = async (url, method, data = {}, headers) => {
-//   const userId = localStorage.getItem("userId");
+// const baseApi = async ({ url = "", method = "GET", data, headers = {} }) => {
+//   // const userId = localStorage.getItem("userId");
+//   const apiUrl = `${BASE_API_URL}/${url}`;
 //   try {
 //     if (nonBodyMethods.includes(method)) {
 //       // GET, DELETE 요청이면 바디 없이 fetch
-//       return await fetch(url, { method, headers });
+//       return await fetch(apiUrl, { method, headers });
+//     } else {
+//       // POST, PUT 요청이면 바디 있는 fetch
+//       return await fetch(apiUrl, {
+//         method,
+//         headers,
+//         credentials: "include"
+//         body: JSON.stringify(data),
+//       });
 //     }
-//     // POST, PUT 요청이면 바디 있는 fetch
-//     return await fetch(url, { method, body: JSON.stringify(data) });
 //   } catch (err) {
-//     console.log("login - try error");
-//     console.log(err);
+//     console.log("error");
+//     console.log(res.status);
 //   }
 // };
